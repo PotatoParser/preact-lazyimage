a7e1cb42c1e30f16b59b47eb6f0f6dbc1175c4a2
PotatoParser <45542237+PotatoParser@users.noreply.github.com>
2022-01-17 13:46:14 -0800

    Add legacy feature

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import { Component, createRef, h } from 'preact';
 
+const GLOBAL_OBSERVER = new Map();
+
+let LEGACY = false;
+
 class LazyImg extends Component {
 	state = { show: false, timer: null }
 	ref = createRef();
@@ -7,39 +11,71 @@ class LazyImg extends Component {
 		entries.forEach(entry => {
 			const { target } = entry;
 			if (entry.isIntersecting) {
-				if (!this.state.show) {
-					if (this.props.delay) {
-						clearTimeout(this.state.timer);
-						const timer = setTimeout(() => {
-							this.setState({ show: true });
-							if (this.props.onLoad) this.props.onLoad(this.ref.current);
-						}, this.props.delay);
-						this.setState({ timer });
-					}
-					else {
-						this.setState({ show: true });
-						if (this.props.onLoad) this.props.onLoad(this.ref.current);
-					}
-				}
+				this.show();
 			} else {
-				if (this.state.show) {
-					clearTimeout(this.state.timer);
-					this.setState({ show: false });
-					if (this.props.onUnload) this.props.onUnload(this.ref.current);
-				}
+				this.hide();
 			}
 		});
 	});
 
+	show() {
+		if (!this.state.show) {
+			if (this.props.delay) {
+				clearTimeout(this.state.timer);
+				const timer = setTimeout(() => {
+					this.setState({ show: true });
+					if (this.props.onLoad) this.props.onLoad(this.ref.current);
+				}, this.props.delay);
+				this.setState({ timer });
+			}
+			else {
+				this.setState({ show: true });
+				if (this.props.onLoad) this.props.onLoad(this.ref.current);
+			}
+		}
+	}
+
+	hide() {
+		if (this.state.show) {
+			clearTimeout(this.state.timer);
+			this.setState({ show: false });
+			if (this.props.onUnload) this.props.onUnload(this.ref.current);
+		}
+	}
+
+	async lazyload() {
+		const H = window.innerHeight;
+		const W = window.innerWidth;
+		const element = this.ref.current;
+		const { top, right, width, height } = element.getBoundingClientRect();
+		const { preloadWidth, preloadHeight } = this.props
+		if (top + height + preloadHeight < 0 || right + width + preloadWidth < 0 || top > H + preloadHeight || right > W + preloadWidth) this.hide();
+		else this.show();
+	}
+
 	observe() {
-		this.observer.observe(this.ref.current);
+		if (!this.props.legacy) this.observer.observe(this.ref.current);
+		else {
+			if (!LEGACY) {
+				LEGACY = true;
+				const observer = async () => {
+					for (const [component, _] of GLOBAL_OBSERVER) component.lazyload();
+				};
+				window.addEventListener('scroll', observer);
+				window.addEventListener('resize', observer);
+			}
+			GLOBAL_OBSERVER.set(this, true);
+			this.lazyload();
+		}
 	}
 
 	unobserve() {
-		this.observer.unobserve(this.ref.current);
+		if (!this.props.legacy) this.observer.unobserve(this.ref.current);
+		else GLOBAL_OBSERVER.delete(this);
 	}
 
 	componentDidMount() {
+		if (!this.props.legacy && (this.props.preloadWidth || this.props.preloadHeight)) throw new Error(`<legacy> property must be true to use preloading`);
 		this.observe();
 	}
 
@@ -49,7 +85,7 @@ class LazyImg extends Component {
 		this.setState({ show: false });
 	}
 
-	render({ src, delay, cache, placeholder, onLoad, onUnload, ...props }, { show }) {
+	render({ src, delay, cache, placeholder, onLoad, onUnload, legacy, preloadWidth, preloadHeight, ...props }, { show }) {
 		return (
 			<img
 				ref={this.ref}
@@ -66,7 +102,10 @@ LazyImg.defaultProps = {
 	delay: 0,
 	src: '',
 	onLoad: false,
-	onUnload: false
+	onUnload: false,
+	legacy: false,
+	preloadWidth: 0,
+	preloadHeight: 0
 };
 
 export default LazyImg;
